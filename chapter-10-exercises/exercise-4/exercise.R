2f2763720f7ac9254b20c332ab721407f2289bca
wendelli-1839314 <wendelli@uw.edu>
2020-01-31 22:55:38 -0800

    week4

--- chapter-10-exercises/exercise-4/exercise.R
+++ chapter-10-exercises/exercise-4/exercise.R
@@ -4,10 +4,10 @@
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
+grants <- read.csv('data/gates_money.csv', stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
-
+View(grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
@@ -21,7 +21,7 @@
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-
+mean(grant$total_amount)
 
 # What was the dollar amount of the largest grant?
 
@@ -30,10 +30,18 @@
 
 
 # Which organization received the largest grant?
+grants[grants$total_amount == max(grants$total_amount), "organization"]
+grants$organization
 
+# Which organization received the smallest grant, and how much was that?
+grants[grants$total_amount == min(grants$total_amount), "organization"]
+grants[grants$total_amount == min(grants$total_amount), c("organization", "total_amount")]
 
-# Which organization received the smallest grant?
+# How many grants over 1000 were awarded in 2010?
+nrow(grants[grants$start_year == "2010", ])
 
-
-# How many grants were awarded in 2010?
+# How much money was awarded in 2018?
+total_amount
+start_year
+sum(grants[grants$start_year == "2010", "total_amount"])
 
--- chapter-11-exercises/exercise-1/exercise.R
+++ chapter-11-exercises/exercise-1/exercise.R
@@ -8,21 +8,22 @@ devtools::install_github("hadley/fueleconomy")
 
 # Use the `libary()` function to load the "fueleconomy" package
 library(fueleconomy)
-
+#install.packages("rlang")
+library(rlang)
 # You should now have access to the `vehicles` data frame
 # You can use `View()` to inspect it
-
+View(vehicles)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+makes <- vehicles$make
 
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set
-
+length(unique(makes))
 
 # Filter the data set for vehicles manufactured in 1997
-
+vehicles_1997 <- vehicles[vehicles$year == 1907, ]
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
@@ -32,17 +33,23 @@ library(fueleconomy)
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+vehicles_1997$average <- (vehicles_1997$hwy + vehicles_1997$cty) / 2
+View(vehicles)
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+vehicles_2wd <- vehicles[vehicles$drive == "2-Wheel Drive", ]
+efficient_2wd <- vehicles_2wd[vehicles_2wd$city > 20, ]
+print(efficient_2wd)
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
+vehicles_2wd[vehicles_2wd$hwy == min(vehicles_2wd$hwy), "id"]
 
+vehicles_2wd$id
+vehicles_2wd$hwy
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters, 
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
