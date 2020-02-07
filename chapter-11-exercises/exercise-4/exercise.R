1f69211ea3db6f8940a8e9be006b14d0b0f8e79d
wendelli-1839314 <wendelli@uw.edu>
2020-02-06 17:26:12 -0800

    week 5

--- chapter-11-exercises/exercise-4/exercise.R
+++ chapter-11-exercises/exercise-4/exercise.R
@@ -11,6 +11,9 @@ library("dplyr")
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
+nrow(flights)
+ncol(flights)
+colnames(flights)
 View(flights)
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
@@ -25,10 +28,12 @@ flights <- arrange(flights, -delayed_in_air)
 
 # For practice, repeat the last 2 steps in a single statement using the pipe
 # operator. You can clear your environmental variables to "reset" the data frame
-
+flights <- flights %>% 
+  mutate(gain_in_air = arr_delay - dep_delay) %>%
+  arrange(desc(gain_in_air))
 
 # Make a histogram of the amount of time gained using the `hist()` function
-
+hist(flights$gain_in_air)
 
 # On average, did flights gain or lose time?
 # Note: use the `na.rm = TRUE` argument to remove NA values from your aggregation
@@ -52,4 +57,4 @@ summarize(to_sea, avg_delayed = mean(delayed_in_air, na.rm = TRUE))
 # Consider flights from JFK to SEA. What was the average, min, and max air time
 # of those flights? Bonus: use pipes to answer this question in one statement
 # (without showing any other data)!
-
+filter(floghts, origin == "JFK")
--- chapter-11-exercises/exercise-6/exercise.R
+++ chapter-11-exercises/exercise-6/exercise.R
@@ -10,9 +10,22 @@ library("dplyr")
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
+avg_delay_df <- flights %>%
+  group_by(dest) %>%
+  summarise(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  rename(faa = dest)
+View(avg_delay_df)
 
+left_join(avg_delay_df, airports, by = "faa") %>%
+  select(faa, avg_arr_delay, name)
 
+View(flights)
+colnames(airlines)
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
 # Which airline had the smallest average arrival delay?
-
+flights %>%
+  group_by(carrier) %>%
+  summarise(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  left_join(airlines, by = "carrier") %>%
+  arrange(-avg_arr_delay)
